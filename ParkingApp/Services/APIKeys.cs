4639e21e2412ce3e08d70ce480fd173ab0abcb16
unknown <dylan.potts16@gmail.com>
2020-11-04 21:09:56 -0500

    Contractor Controller - added logic attaching user id to created profile. Cleaned up Contractor Edit, Create and Details View. Added Geocoding service class and IGeocoding Interface. Added Transient Geocoding service to startup page.

---
+++ ParkingApp/Services/APIKeys.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkingApp.Services
+{
+    public static class APIKeys
+    {
+        public const string GOOGLE_API_KEY = "AIzaSyAHd7Ww9Is3mNz6JKsMVs1-2fPIqfWa3iA";
+    }
+}
