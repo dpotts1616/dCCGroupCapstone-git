1f016ffceb3535e8bd2b4ff361b6985fe4e825c3
Brooke McBain <mcbainbrooke@gmail.com>
2020-11-05 21:17:26 -0600

    fixed title and css properties

--- ParkingApp/wwwroot/js/site.js
+++ ParkingApp/wwwroot/js/site.js
@@ -1,5 +1,5 @@
 ﻿// function([string1, string2],target id,[color1,color2])    
-consoleText(['WAIP', 'Where Am I Going?'], 'text', ['yellow', 'yellow']);
+consoleText(['WAIP', 'Where Am I Parking?'], 'text', ['yellow', 'yellow']);
 
 function consoleText(words, id, colors) {
     if (colors === undefined) colors = ['#fff'];
