cce1297bba980608bb131a937d1a8c53bc07b647
unknown <bridgetott@yahoo.com>
2020-11-09 11:50:38 -0600

    change some buttons in register page

---
+++ ParkingApp/Migrations/20201109150826_again.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParkingApp.Migrations
+{
+    public partial class again : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartTime",
+                table: "Reservations",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndTime",
+                table: "Reservations",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1bd87b74-3b02-4126-98e3-b9acb0767fc6",
+                column: "ConcurrencyStamp",
+                value: "d4282346-7ce1-414b-8243-62c5d76eed93");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "77e60802-3e18-40cf-8999-79aa642defb1",
+                column: "ConcurrencyStamp",
+                value: "a0943a45-ee7f-464a-92ac-bfa87c06448b");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "StartTime",
+                table: "Reservations",
+                type: "time",
+                nullable: false,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "EndTime",
+                table: "Reservations",
+                type: "time",
+                nullable: false,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1bd87b74-3b02-4126-98e3-b9acb0767fc6",
+                column: "ConcurrencyStamp",
+                value: "80a5ce22-a6f8-474e-9205-980bf4fbb67e");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "77e60802-3e18-40cf-8999-79aa642defb1",
+                column: "ConcurrencyStamp",
+                value: "e4438d2b-8796-4f79-9a2c-ff032f250f65");
+        }
+    }
+}
